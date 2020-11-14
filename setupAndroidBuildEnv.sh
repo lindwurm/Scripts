b96111004bc4acf66b0164ba0b8ad30ebe036149
lindwurm <lindwurm.q@gmail.com>
2020-11-14 15:21:37 +0900

    python-is-python3

--- setupAndroidBuildEnv.sh
+++ setupAndroidBuildEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool lib32ncurses-dev libncurses5 libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools unzip xsltproc zip zlib1g-dev
+sudo apt update && sudo apt upgrade -y && sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool lib32ncurses-dev libncurses5 libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush python-is-python3 rsync schedtool squashfs-tools unzip xsltproc zip zlib1g-dev
 git config --global user.name "android"
 git config --global user.email "android"
 mkdir ~/bin
